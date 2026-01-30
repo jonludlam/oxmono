7cb55844975f4971d7e31b17abc5eab932597b73
Anil Madhavapeddy <anil@recoil.org>
2026-01-30 10:33:12 +0000

    Import stdio.v0.18~preview.130 83+317
    Source: https://github.com/janestreet/stdio

---
+++ opam/stdio/src/stdio.mli
@@ -0,0 +1,35 @@
+@@ portable
+
+open! Base
+module In_channel = In_channel
+module Out_channel = Out_channel
+
+(** Same as {!In_channel.stdin} *)
+val stdin : In_channel.t
+
+(** Same as {!Out_channel.stdout} *)
+val stdout : Out_channel.t
+
+(** Same as {!Out_channel.stderr} *)
+val stderr : Out_channel.t
+
+(** Same as {!Out_channel.printf} *)
+val printf : ('a, Out_channel.t, unit) format -> 'a
+
+(** Same as {!Out_channel.print_s} *)
+val print_s : ?mach:unit -> Sexp.t @ local -> unit
+
+(** Same as {!Out_channel.eprint_s} *)
+val eprint_s : ?mach:unit -> Sexp.t @ local -> unit
+
+(** Same as {!Out_channel.eprintf} *)
+val eprintf : ('a, Out_channel.t, unit) format -> 'a
+
+(** Same as {!Out_channel.print_string} *)
+val print_string : string @ local -> unit
+
+(** Same as {!Out_channel.print_endline} *)
+val print_endline : string @ local -> unit
+
+(** Same as {!Out_channel.prerr_endline} *)
+val prerr_endline : string @ local -> unit
